850d4cca42470bac12f9053c27b2f0540dbb0372
thureintun234 <thurein12456@gmail.com>
2021-10-09 16:20:19 +0630

    fix errors

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import styled from 'styled-components';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GithubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Particle from '../Components/Particle';
+
+function HomePage() {
+    return (
+        <HomePageStyled>
+            <div className="particle-con">
+                <Particle />
+            </div>
+            <div className="typography">
+                <h1>Hi, I'm <span>Thu Rein Tun</span></h1>
+                <p>
+                    I am a person who love problem solving and studying
+                    new technology. I like to make projects with teams.
+                </p>
+                <div className="icons">
+                    <a href="https://codepen.io/pen/" className="icon i-facebook">
+                        <FacebookIcon />
+                    </a>
+                    <a href="https://codepen.io/pen/" className="icon i-github">
+                        <GithubIcon />
+                    </a>
+                    <a href="https://codepen.io/pen/" className="icon i-twitter">
+                        <TwitterIcon />
+                    </a>
+                </div>
+            </div>
+        </HomePageStyled>
+    )
+}
+
+const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
+   
+    .typography{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        width: 80%;
+
+        .icons{
+            display: flex;
+            justify-content: center;
+            margin-top: 1rem;
+            .icon{
+                border: 2px solid var(--border-color);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                border-radius: 50%;
+                transition: all .4s ease-in-out;
+                cursor: pointer;
+                &:hover{
+                    border: 2px solid var(--primary-color);
+                    color: var(--primary-color);
+                }
+                &:not(:last-child){
+                    margin-right: 1rem;
+                }
+                svg{
+                    margin: .5rem;
+                }
+            }
+
+            .i-twitter{
+                &:hover{
+                    border: 2px solid blue;
+                    color: blue;
+                }
+            }
+            .i-github{
+                &:hover{
+                    border: 2px solid #5F4687;
+                    color: #5F4687;
+                }
+            }
+        }
+    }
+`;
+
+export default HomePage;
